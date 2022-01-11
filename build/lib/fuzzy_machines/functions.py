993c2dc8d6efb1b2d423076f7846eacd504d6d8d
Caio Castro <lcastro.caio@gmail.com>
2022-01-11 15:40:13 -0300

    new build

---
+++ build/lib/fuzzy_machines/functions.py
@@ -0,0 +1,16 @@
+""" fuzzy logic functions """
+
+
+def convert(my_name):
+    """
+    Print a line about converting a notebook.
+    Args:
+        my_name (str): person's name
+    Returns:
+        None
+    """
+    if not isinstance(my_name, str):
+        raise TypeError(
+            f'arg "my_name" expected type str. Received type {type(my_name)}'
+        )
+    print(f"I'll convert a notebook for you some day, {my_name}.")
